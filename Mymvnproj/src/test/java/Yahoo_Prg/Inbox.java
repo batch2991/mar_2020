cacb800373f0848a05ec8268e93157a6da553b6d
Ashraf Mohammed <Ashraf Mohammed@DESKTOP-U5KCFUO>
2020-04-18 19:17:36 +0530

    Inbox progm has updated

--- Mymvnproj/src/test/java/Yahoo_Prg/Inbox.java
+++ Mymvnproj/src/test/java/Yahoo_Prg/Inbox.java
@@ -13,7 +13,7 @@ public class Inbox extends MainClass
 	  driver.findElement(By.xpath(InboxP.xcheckbox)).click();
 	  driver.findElement(By.xpath(InboxP.xdelete)).click();
 
-	  System.out.println("Hello Inbox");
+	  System.out.println("Hello Java 1");
 
 	  System.out.println("Hello");
 	  System.out.println("hai");
